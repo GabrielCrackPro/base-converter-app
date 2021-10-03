a93b3798e5b47db04231f8f993ae32a354b9013b
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-10-03 14:54:15 +0200

    Code refactored and styles updated 🔧

--- public/app.js
+++ public/app.js
@@ -7,16 +7,20 @@ const formatError = document.querySelector(".format-error");
 formatError.style.display = "none";
 
 const convert = (number, base) => {
-  /* if (isNaN(number) || number == "") {
+  if (
+    isNaN(number) ||
+    number == "" ||
+    number == null ||
+    number.toString().length > base
+  ) {
     formatError.style.display = "block";
     formatError.classList.add("shake");
     formatError.style.backgroundColor = "#FFD523";
     formatError.innerHTML =
       "<i class='fas fa-times'></i> Please enter a valid number";
     return "";
-  } */
-  // TODO:Handle hex inputs
-  return parseInt(number).toString(base);
+  }
+  return parseInt(number).toString(base); //TODO: Handle letters in base 16
 };
 
 forms.forEach((form) => {
@@ -30,8 +34,6 @@ forms.forEach((form) => {
         inputs[1].value = convert(value, 2); // binary input
         inputs[2].value = convert(value, 8); // octal input
         inputs[3].value = convert(value, 16); // hexadecimal input
-
-        console.log(inputs);
         break;
       case "binary":
         value = formData.get("binary");
@@ -42,10 +44,10 @@ forms.forEach((form) => {
         break;
       case "hex":
         value = formData.get("hex");
-        inputs[0].value = parseInt(value).toString(10); // decimal input
-        inputs[1].value = parseInt(value).toString(2); // binary input
+        inputs[0].value = convert(value, 10); // decimal input
+        inputs[1].value = convert(value, 2); // binary input
         inputs[2].value = convert(value, 8); // octal input
-        inputs[3].value = parseInt(value); // hexadecimal input
+        inputs[3].value = convert(value, 16); // hexadecimal input
         break;
       case "octal":
         value = formData.get("octal");
